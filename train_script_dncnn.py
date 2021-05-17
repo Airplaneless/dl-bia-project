e221b60f2e14ddaba6517dab7faa79b7dadae825
NevVerVer <1c2c3c4c5c6v>
2021-05-17 17:50:42 +0300

    dncnn

---
+++ train_script_dncnn.py
@@ -0,0 +1,173 @@
+import os
+import sys
+import numpy as np
+import h5py
+import pylab as plt
+import torch
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import Callback, ModelCheckpoint
+from k_space_reconstruction.nets.dncnn import DnCNNModule
+from k_space_reconstruction.datasets.fastmri import FastMRITransform, FastMRIh5Dataset, RandomMaskFunc
+from k_space_reconstruction.utils.metrics import pt_msssim, pt_ssim, ssim, nmse, psnr
+from k_space_reconstruction.utils.loss import l1_loss, compund_mssim_l1_loss
+from k_space_reconstruction.utils.kspace import spatial2kspace, kspace2spatial
+
+print('Available GPUs: ', torch.cuda.device_count())
+torch.manual_seed(42)
+np.random.seed(42)
+
+
+batch_size = 64
+accum_val = 2
+#------noiseless----------------------------------------
+
+# transform = FastMRITransform(
+#     RandomMaskFunc([0.08], [4]),
+#     noise_level=400,
+#     noise_type='none'
+# )
+
+# train_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/train.h5', transform)
+# val_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/val.h5', transform)
+# train_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=12, shuffle = True)
+# val_generator = torch.utils.data.DataLoader(val_dataset, batch_size=1, num_workers=12, shuffle = True)
+
+net = DnCNNModule(
+    dncnn_chans=64, 
+    dncnn_depth=10, 
+    criterion=compund_mssim_l1_loss, 
+    verbose_batch=50, 
+    optimizer='Adam',
+    lr=1e-4,
+    lr_step_size=5,
+    lr_gamma=0.2,
+    weight_decay=0.0
+)
+
+# trainer = pl.Trainer(
+#     gpus=1, max_epochs=20,
+#     accumulate_grad_batches=accum_val,
+#     terminate_on_nan=True, 
+#     default_root_dir='logs/DnCNN',
+#     callbacks=[
+#         pl.callbacks.ModelCheckpoint(
+#             save_last=True,
+#             save_top_k=4, 
+#             monitor='val_loss', 
+#             filename='{epoch}-{ssim:.4f}-{psnr:.4f}-{nmse:.5f}'
+#         ),
+#         pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+#         pl.callbacks.GPUStatsMonitor(temperature=True)
+#     ]
+# )
+
+# trainer.fit(net, train_dataloader=train_generator, val_dataloaders=val_generator)
+# torch.save(net.net.state_dict(), 'dncnn-noiseless.pth')
+# print('Succsesfully saved NOISELESS model')
+
+#----------------gaussian-----------------------------------------
+net.net.load_state_dict(torch.load('dncnn-noiseless.pth'))
+
+transform = FastMRITransform(
+    RandomMaskFunc([0.08], [4]),
+#     noise_level=400,
+    noise_level=100,
+    noise_type='normal'
+)
+
+train_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/train.h5', transform)
+val_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/val.h5', transform)
+train_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=12, shuffle = True)
+val_generator = torch.utils.data.DataLoader(val_dataset, batch_size=1, num_workers=12, shuffle = True)
+
+trainer = pl.Trainer(
+    gpus=1, max_epochs=10,
+    accumulate_grad_batches=accum_val,
+    terminate_on_nan=True, 
+    default_root_dir='logs/DnCNN_gaussian',
+    callbacks=[
+        pl.callbacks.ModelCheckpoint(
+            save_last=True,
+            save_top_k=4, 
+            monitor='val_loss', 
+            filename='{epoch}-{ssim:.4f}-{psnr:.4f}-{nmse:.5f}'
+        ),
+        pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+        pl.callbacks.GPUStatsMonitor(temperature=True)
+    ]
+);
+
+trainer.fit(net, train_dataloader=train_generator, val_dataloaders=val_generator)
+torch.save(net.net.state_dict(), 'dncnn-with-gaussian.pth')
+
+print('Succsesfully saved GAUSSIAN model')
+
+#----------------salt------------------------------------------------
+net.net.load_state_dict(torch.load('dncnn-noiseless.pth'))
+transform = FastMRITransform(
+    RandomMaskFunc([0.08], [4]),
+    noise_level=5e4,
+    noise_type='salt'
+)
+
+train_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/train.h5', transform)
+val_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/val.h5', transform)
+train_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=12, shuffle = True)
+val_generator = torch.utils.data.DataLoader(val_dataset, batch_size=1, num_workers=12, shuffle = True)
+
+trainer = pl.Trainer(
+    gpus=1, max_epochs=10,
+    accumulate_grad_batches=accum_val,
+    terminate_on_nan=True, 
+    default_root_dir='logs/DnCNN_salt',
+    callbacks=[
+        pl.callbacks.ModelCheckpoint(
+            save_last=True,
+            save_top_k=4, 
+            monitor='val_loss', 
+            filename='{epoch}-{ssim:.4f}-{psnr:.4f}-{nmse:.5f}'
+        ),
+        pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+        pl.callbacks.GPUStatsMonitor(temperature=True)
+    ]
+);
+
+trainer.fit(net, train_dataloader=train_generator, val_dataloaders=val_generator)
+torch.save(net.net.state_dict(), 'dncnn-with-salt.pth')
+print('Succsesfully saved SALT model')
+
+#----------------both-------------------------------------------------
+net.net.load_state_dict(torch.load('dncnn-noiseless.pth'))
+transform = FastMRITransform(
+    RandomMaskFunc([0.08], [4]),
+    noise_level=5e4,
+    noise_type='normal_and_salt'
+)
+
+train_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/train.h5', transform)
+val_dataset = FastMRIh5Dataset('small_fastmri_pd_3t/val.h5', transform)
+train_generator = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=12, shuffle = True)
+val_generator = torch.utils.data.DataLoader(val_dataset, batch_size=1, num_workers=12, shuffle = True)
+
+trainer = pl.Trainer(
+    gpus=1, max_epochs=10,
+    accumulate_grad_batches=accum_val,
+    terminate_on_nan=True, 
+    default_root_dir='logs/DnCNN_gaussian_and_salt',
+    callbacks=[
+        pl.callbacks.ModelCheckpoint(
+            save_last=True,
+            save_top_k=4, 
+            monitor='val_loss', 
+            filename='{epoch}-{ssim:.4f}-{psnr:.4f}-{nmse:.5f}'
+        ),
+        pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+        pl.callbacks.GPUStatsMonitor(temperature=True)
+    ]
+);
+
+trainer.fit(net, train_dataloader=train_generator, val_dataloaders=val_generator)
+torch.save(net.net.state_dict(), 'dncnn-with-gaussian-salt.pth')
+print('Succsesfully saved BOTH model')
+print('All done')
